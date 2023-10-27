694b2f8d108b32a5ce0d84668f8771f5a84ca02c
sirkotal <lordkotal2002@gmail.com>
2023-10-27 15:13:10 +0100

    Added a Utils file

---
+++ src/utils.pl
@@ -0,0 +1,15 @@
+replace_in_row([_|Rest], 1, Val, [Val|Rest]).
+replace_in_row([X|Rest], Col, Val, [X|NewRest]) :-
+    Col > 1,
+    NextCol is Col - 1,
+    replace_in_row(Rest, NextCol, Val, NewRest).
+
+replace([Row|Rest], 1, Col, Val, [NewRow|Rest]) :-
+    replace_in_row(Row, Col, Val, NewRow).
+replace([Row|Rest], RowIndex, Col, Val, [Row|NewRest]) :-
+    RowIndex > 1,
+    NextIndex is RowIndex - 1,
+    replace(Rest, NextIndex, Col, Val, NewRest).
+
+remove_piece(Board, X, Y, NewBoard) :-
+    replace(Board, X, Y, empty, NewBoard).
