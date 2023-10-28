78b4b6ee3f97d19336ad1168e69102f5dfde6ad8
joaoprcoutinho <up202108787@up.pt>
2023-10-28 16:53:57 +0100

    menu + refactor on count_adjacents

--- src/logic.pl
+++ src/logic.pl
@@ -1,14 +1,6 @@
 :- use_module(library(lists)).
 
-
-/*maybe move to utils*/
-print_array([]). % Base case, the list is empty, nothing to print.
-
-print_array([Head | Tail]) :-
-    write(Head), % Print the current element
-    nl,          % Print a newline character to separate elements
-    print_array(Tail). % Recursively print the rest of the list
-
+/*move to utils*/
 get_element(Board, Row, Column, Element) :-
     nth1(Row, Board, RowList),
     nth1(Column, RowList, Element).
@@ -17,63 +9,41 @@ count_adjacents(Position_row, Position_column, Board, Color, Total, InitialVisit
     append([[Position_row, Position_column]], InitialVisited, Temporary),
     (Position_column < 10 ->
       NewPosition_column is Position_column + 1,
-      get_element(Board, Position_row, NewPosition_column, Element1),
-      ((Element1 = Color, \+ memberchk([Position_row, NewPosition_column], InitialVisited)) -> 
-      	NewInitialVisitedRight = Temporary,
-        count_adjacents(Position_row, NewPosition_column, Board, Color, NewTotalRight, NewInitialVisitedRight, NewEndVisitedRight);  
-      	NewTotalRight = 0, NewEndVisitedRight = Temporary)
+      get_element(Board, Position_row, NewPosition_column, ElementRight),
+      ((ElementRight = Color, \+ memberchk([Position_row, NewPosition_column], InitialVisited)) -> 
+        InitialVisitedRight = Temporary,
+        count_adjacents(Position_row, NewPosition_column, Board, Color, TotalRight, InitialVisitedRight, EndVisitedRight);  
+        TotalRight = 0, EndVisitedRight = Temporary)
     ),
-    TotalRight = NewTotalRight,
-    VisitedRight = NewEndVisitedRight,
-    
+      
     (Position_column > 1 ->
       NewPosition_column1 is Position_column - 1,
-      get_element(Board, Position_row, NewPosition_column1, Element2),
-      ((Element2 = Color, \+ memberchk([Position_row, NewPosition_column1], VisitedRight)) ->
-        NewInitialVisitedLeft = VisitedRight,
-      	count_adjacents(Position_row, NewPosition_column1, Board, Color, NewTotalLeft, NewInitialVisitedLeft, NewEndVisitedLeft);  
-      	NewTotalLeft = 0, NewEndVisitedLeft = VisitedRight)
+      get_element(Board, Position_row, NewPosition_column1, ElementLeft),
+      ((ElementLeft = Color, \+ memberchk([Position_row, NewPosition_column1], EndVisitedRight)) ->
+        InitialVisitedLeft = EndVisitedRight,
+        count_adjacents(Position_row, NewPosition_column1, Board, Color, TotalLeft, InitialVisitedLeft, EndVisitedLeft);  
+        TotalLeft = 0, EndVisitedLeft = EndVisitedRight)
     ),
-    TotalLeft = NewTotalLeft,
-    VisitedLeft = NewEndVisitedLeft,
 
-	(Position_row < 10 ->
-      NewPosition_row is Position_row + 1,
-      get_element(Board, NewPosition_row, Position_column, Element3),
-      ((Element3 = Color, \+ memberchk([NewPosition_row, Position_column], VisitedLeft)) ->
-        NewInitialVisitedBelow = VisitedLeft,
-      	count_adjacents(NewPosition_row, Position_column, Board, Color, NewTotalBelow, NewInitialVisitedBelow, NewEndVisitedBelow);  
-      	NewTotalBelow = 0, NewEndVisitedBelow = VisitedLeft)
+    (Position_row < 10 ->
+        NewPosition_row is Position_row + 1,
+        get_element(Board, NewPosition_row, Position_column, ElementBelow),
+        ((ElementBelow = Color, \+ memberchk([NewPosition_row, Position_column], EndVisitedLeft)) ->
+          InitialVisitedBelow = EndVisitedLeft,
+          count_adjacents(NewPosition_row, Position_column, Board, Color, TotalBelow, InitialVisitedBelow, EndVisitedBelow);  
+          TotalBelow = 0, EndVisitedBelow = EndVisitedLeft)
     ),
-    TotalBelow = NewTotalBelow,
-    VisitedBelow = NewEndVisitedBelow,
-    
+      
     (Position_row > 1 ->
       NewPosition_row1 is Position_row - 1,
-      get_element(Board, NewPosition_row1, Position_column, Element4),
-      ((Element4 = Color, \+ memberchk([NewPosition_row1, Position_column], VisitedBelow)) ->
-        NewInitialVisitedAbove = VisitedBelow,
-      	count_adjacents(NewPosition_row1, Position_column, Board, Color, NewTotalAbove, NewInitialVisitedAbove, NewEndVisitedAbove);  
-      	NewTotalAbove = 0, NewEndVisitedAbove = VisitedBelow)
+      get_element(Board, NewPosition_row1, Position_column, ElementAbove),
+      ((ElementAbove = Color, \+ memberchk([NewPosition_row1, Position_column], EndVisitedBelow)) ->
+        InitialVisitedAbove = EndVisitedBelow,
+        count_adjacents(NewPosition_row1, Position_column, Board, Color, TotalAbove, InitialVisitedAbove, EndVisitedAbove);  
+        TotalAbove = 0, EndVisitedAbove = EndVisitedBelow)
     ),
-    TotalAbove = NewTotalAbove,
-    VisitedAbove = NewEndVisitedAbove,
 
     Total is TotalAbove + TotalBelow + TotalLeft + TotalRight + 1,
-    
-	EndVisited = VisitedAbove.
-
-initialize_board([
-        [border, border, border, border, border, border, border, border, border, border],
-        [border, empty, blue, blue, blue, blue, blue, blue, blue, border],
-        [border, blue, blue, blue, red, blue, red, blue, red, border],
-        [border, red, blue, red, blue, red, blue, red, blue, border],
-        [border, blue, red, blue, red, blue, red, blue, red, border],
-        [border, red, blue, red, blue, red, blue, red, blue, border],
-        [border, blue, red, blue, red, blue, red, blue, red, border],
-        [border, red, blue, red, blue, red, blue, red, blue, border],
-        [border, empty, red, blue, red, blue, red, blue, empty, border],
-        [border, border, border, border, border, border, border, border, border, border]
-]).
-
+      
+    EndVisited = EndVisitedAbove.
 
--- src/menu.pl
+++ src/menu.pl
@@ -1,25 +1,38 @@
-show_menu :-write('----- BOUNCE -----'), nl,
-            write(' '), nl,
-            write('1 - PLAY'), nl,
-            write(' '), nl,
-            write('2 - RULES'), nl,
-            write(' '), nl,
-            write('3 - EXIT'), nl,
-            write(' '), nl.
+show_menu :-
+    write('----- BOUNCE -----'), nl,
+    write(' '), nl,
+    write('1 - PLAY'), nl,
+    write(' '), nl,
+    write('2 - RULES'), nl,
+    write(' '), nl,
+    write('3 - EXIT'), nl,
+    write(' '), nl.
 
+read_menu(X) :- 
+    repeat,
+    write('INSERT AN OPTION'), nl,
+    read(X),
+    ((X = 1 ; X = 2 ; X = 3) -> option(X) ; (write('WRONG OPTION, PLEASE ENTER ANOTHER ONE'), nl, fail)).
 
+option(1) :- 
+    write('GAME').
 
-read_menu(X) :- repeat,
-                write('INSERT AN OPTION'), nl,
-                read(X),
-                ((X = 1 ; X = 2 ; X = 3) -> option(X) ; (write('WRONG OPTION, PLEASE ENTER ANOTHER ONE'), nl, fail)).
+option(2) :- 
+    write('INTRODUCTION: Bounce is a two-player game played on a square board of any even size. The board is initially filled with a checkerboard pattern of red and blue checkers, except the corner squares, which are unoccupied. Mark Steere designed Bounce in August 2023.'), nl,
+    write(''), nl,
+    write('GROUP: A group here is a monocolored, orthogonally interconnected group of checkers'), nl,
+    write(''), nl,
+    write('OBJECT: If, at the conclusion of your turn, all of your checkers are in one group, you win.'), nl,
+    write(''), nl,
+    write('PLAY: Starting with Red, players take turns moving one of their checkers to an unoccupied square (if they have a legal move available. See CHECKER REMOVAL below.)'), nl,
+    write(''), nl,
+    write('The checker you move must be part of a larger group after your move than it was before your move. '), nl,
+    write(''), nl,
+    write('CHECKER REMOVAL: If you don\'t have any legal moves availableon your turn, you must instead remove any one of your checkers from the board, concluding your turn'), nl,
+    repeat,
+    write(''), nl,
+    write('INSERT 1 TO GO BACK TO MENU'), nl,
+    read(Y),
+    ((Y = 1) -> show_menu, read_menu(_) ; (write('WRONG OPTION, PLEASE ENTER 1 TO GO BACK'), nl, fail)).
 
-option(1) :- write('GAME').
-
-option(2) :- write('RULES').
-
-option(3) :- write('LEFT').
-
-
-
-              
+option(3) :- halt(0).
